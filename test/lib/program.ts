cb9ef01b5d11601c5439c7d09ef8d85d3d66e967
David Herron <david@davidherron.com>
2023-12-04 23:13:39 +0200

    Initial revision

---
+++ test/lib/program.ts
@@ -0,0 +1,160 @@
+
+
+import { assert } from 'chai';
+import * as path from 'path';
+import * as url from 'url';
+import util from 'node:util';
+
+export const __filename = url.fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+import { promises as fsp } from 'node:fs';
+import {
+    Program, programSchema
+} from '../../package/dist/index.js';
+// from 'openadr-3-ts-types';
+import YAML from 'js-yaml';
+
+describe('PROGRAM', function() {
+    let data;
+    before(async function() {
+
+        const file = await fsp.readFile(
+            path.join(__dirname, '..', 'data', 'program.yml'),
+            'utf8');
+        const doc: any = YAML.load(file);
+        data = doc;
+    });
+
+    let prog0: Program;
+    it('should parse prog0', function() {
+        prog0 = programSchema.parse(data.prog0[0]) as Program;
+        // console.log(prog0);
+        assert.deepEqual(prog0, {
+            programName: 'TEST-EXAMPLE-1',
+            programLongName: 'TEST Example 1 for ESX service',
+            retailerName: 'TEST-LTP',
+            retailerLongName: 'TEST Long Tail Pipe',
+            programType: 'any',
+            country: 'US',
+            principalSubdivision: 'SD',
+            timeZoneOffset: '-PT8H',
+            intervalPeriod: {
+                start: '2023-02-20T00:00:00Z',
+                duration: 'P3M'
+            },
+            programDescriptions: null,
+            bindingEvents: false,
+            localPrice: false,
+            payloadDescriptors: null,
+            targets: null
+        });
+    });
+
+    it('should fail to parse bad program', function() {
+        let didFail = false;
+        try {
+            const badprog
+                = programSchema.parse(data.BADprog[0]);
+        } catch (err) {
+            didFail = true;
+            assert.deepEqual(err.issues, [
+                {
+                  code: 'invalid_type',
+                  expected: 'string',
+                  received: 'number',
+                  path: ["programName"],
+                  message: 'Expected string, received number'
+                },
+                {
+                  code: 'invalid_type',
+                  expected: 'string',
+                  received: 'number',
+                  path: ["programLongName"],
+                  message: 'Expected string, received number'
+                },
+                {
+                  code: 'invalid_type',
+                  expected: 'string',
+                  received: 'number',
+                  path: ["retailerName"],
+                  message: 'Expected string, received number'
+                },
+                {
+                  code: 'invalid_type',
+                  expected: 'string',
+                  received: 'number',
+                  path: ["retailerLongName"],
+                  message: 'Expected string, received number'
+                },
+                {
+                  validation: 'regex',
+                  code: 'invalid_string',
+                  message: 'Invalid',
+                  path: ["timeZoneOffset"]
+                },
+                {
+                  code: 'invalid_type',
+                  expected: 'boolean',
+                  received: 'string',
+                  path: ["bindingEvents"],
+                  message: 'Expected boolean, received string'
+                },
+                {
+                  code: 'invalid_type',
+                  expected: 'boolean',
+                  received: 'string',
+                  path: ["localPrice"],
+                  message: 'Expected boolean, received string'
+                }
+              ]);
+        }
+        assert.isOk(didFail);
+    })
+
+    it('should fail to parse zero-length program name', function() {
+        let didFail = false;
+        try {
+            const badprog
+                = programSchema.parse(data.BADprog[1]);
+        } catch (err) {
+            didFail = true;
+            assert.deepEqual(err.issues, [
+                {
+                    code: 'too_small',
+                    minimum: 1,
+                    type: 'string',
+                    inclusive: true,
+                    exact: false,
+                    message: 'String must contain at least 1 character(s)',
+                    path: ["programName"]
+                }
+              ]);
+        }
+        assert.isOk(didFail);
+    });
+
+    it('should fail to parse extra-long program name', function() {
+        let didFail = false;
+        try {
+            const badprog
+                = programSchema.parse(data.BADprog[2]);
+        } catch (err) {
+            didFail = true;
+            assert.deepEqual(err.issues, [
+                {
+                    code: 'too_big',
+                    maximum: 128,
+                    type: 'string',
+                    inclusive: true,
+                    exact: false,
+                    message: 'String must contain at most 128 character(s)',
+                    path: ["programName"]
+                  }
+              ]);
+        }
+        assert.isOk(didFail);
+    });
+
+});
+
---
+++ test/lib/report.ts
@@ -0,0 +1,305 @@
+
+import { assert } from 'chai';
+import * as path from 'path';
+import * as url from 'url';
+import util from 'node:util';
+
+export const __filename = url.fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+import { promises as fsp } from 'node:fs';
+import {
+    Report, reportSchema
+} from '../../package/dist/index.js';
+// from 'openadr-3-ts-types';
+import YAML from 'js-yaml';
+
+
+describe('REPORT', function() {
+    let data;
+    before(async function() {
+
+        const file = await fsp.readFile(
+            path.join(__dirname, '..', 'data', 'report.yml'),
+            'utf8');
+        const doc: any = YAML.load(file);
+        data = doc;
+    });
+
+    let report: Report;
+    it('should parse report', function() {
+        report = reportSchema.parse(data.reports[0]) as Report;
+        assert.deepEqual(report, {
+            programID: '0',
+            eventID: '0',
+            clientName: 'VEN99',
+            reportName: 'Test report #1',
+            payloadDescriptors: [
+              {
+                objectType: 'REPORT_PAYLOAD_DESCRIPTOR',
+                payloadType: 'USAGE',
+                readingType: null,
+                units: null,
+                accuracy: null,
+                confidence: 100
+              }
+            ],
+            resources: [
+              {
+                resourceName: '99',
+                intervalPeriod: {
+                    start: "2023-03-11T12:23:15Z",
+                    duration: "PT1M"
+                },
+                intervals: [
+                    {
+                        id: 0,
+                        intervalPeriod: {
+                            duration: "PT15M",
+                            start: "2023-03-26T18:30:28.829Z"
+                        },
+                        payloads: [
+                            {
+                              type: "USAGE",
+                              values: [
+                                94
+                              ]
+                            }
+                        ]
+                    }
+                ]
+              },
+              {
+                resourceName: '99',
+                intervalPeriod: {
+                    start: "2023-03-11T12:23:15Z",
+                    duration: "PT1M"
+                },
+                intervals: [
+                    {
+                        id: 0,
+                        intervalPeriod: {
+                            duration: "PT15M",
+                            start: "2023-03-26T18:30:28.829Z"
+                        },
+                        payloads: [
+                            {
+                              type: "USAGE",
+                              values: [
+                                94
+                              ]
+                            }
+                        ]
+                    }
+                ]
+              }
+            ]
+        });
+
+    });
+
+    it('should parse report with passthrough', function() {
+        let report2: any = reportSchema.passthrough().parse(data.reports[0]) as Report;
+        // console.log(report2);
+        assert.deepEqual(report2, {
+            // This field is in the data, and is not
+            // part of the defined Program object.
+            // For the code to compile, I had to make
+            // the type 'any'.
+            ignoreVenNotFound: true,
+            programID: '0',
+            eventID: '0',
+            clientName: 'VEN99',
+            reportName: 'Test report #1',
+            payloadDescriptors: [
+              {
+                objectType: 'REPORT_PAYLOAD_DESCRIPTOR',
+                payloadType: 'USAGE',
+                readingType: null,
+                units: null,
+                accuracy: null,
+                confidence: 100
+              }
+            ],
+            resources: [
+              {
+                resourceName: '99',
+                intervalPeriod: {
+                    start: "2023-03-11T12:23:15Z",
+                    duration: "PT1M"
+                },
+                intervals: [
+                    {
+                        id: 0,
+                        intervalPeriod: {
+                            duration: "PT15M",
+                            start: "2023-03-26T18:30:28.829Z"
+                        },
+                        payloads: [
+                            {
+                              type: "USAGE",
+                              values: [
+                                94
+                              ]
+                            }
+                        ]
+                    }
+                ]
+              },
+              {
+                resourceName: '99',
+                intervalPeriod: {
+                    start: "2023-03-11T12:23:15Z",
+                    duration: "PT1M"
+                },
+                intervals: [
+                    {
+                        id: 0,
+                        intervalPeriod: {
+                            duration: "PT15M",
+                            start: "2023-03-26T18:30:28.829Z"
+                        },
+                        payloads: [
+                            {
+                              type: "USAGE",
+                              values: [
+                                94
+                              ]
+                            }
+                        ]
+                    }
+                ]
+              }
+            ]
+        });
+
+    });
+
+    it('should fail to parse report with strict', function() {
+        let didFail = false;
+        try {
+            let report2: any = reportSchema.strict().parse(data.reports[0]) as Report;
+        } catch (err) {
+            // console.log(err);
+            didFail = true;
+            assert.deepEqual(err.issues, [
+                {
+                    code: 'unrecognized_keys',
+                    keys: ["ignoreVenNotFound"],
+                    path: [],
+                    message: "Unrecognized key(s) in object: 'ignoreVenNotFound'"
+                  }
+            ])
+        }
+        assert.isOk(didFail);
+    });
+
+    it('should fail to parse bad report', function() {
+        let didFail = false;
+        try {
+            const badprog
+                = reportSchema.parse(data.BADreports[0]);
+        } catch (err) {
+            didFail = true;
+            // console.log(err);
+            assert.deepEqual(err.issues, [
+                {
+                    code: 'invalid_type',
+                    expected: 'string',
+                    received: 'number',
+                    path: ["programID"],
+                    message: 'Expected string, received number'
+                  },
+                  {
+                    code: 'invalid_type',
+                    expected: 'string',
+                    received: 'number',
+                    path: ["eventID"],
+                    message: 'Expected string, received number'
+                  },
+                  {
+                    code: 'invalid_type',
+                    expected: 'string',
+                    received: 'number',
+                    path: ["reportName"],
+                    message: 'Expected string, received number'
+                  },
+                  {
+                    code: 'invalid_type',
+                    expected: 'string',
+                    received: 'number',
+                    path: ["resources", 0, "resourceName"],
+                    message: 'Expected string, received number'
+                  },
+                  {
+                    code: 'invalid_type',
+                    expected: 'number',
+                    received: 'string',
+                    path: ["resources", 0, "intervals", 0, "id"],
+                    message: 'Expected number, received string'
+                  },
+                  {
+                    code: 'invalid_type',
+                    expected: 'string',
+                    received: 'number',
+                    path: ["resources", 1, "resourceName"],
+                    message: 'Expected string, received number'
+                  },
+                  {
+                    code: 'invalid_type',
+                    expected: 'number',
+                    received: 'string',
+                    path: ["resources", 1, "intervals", 0, "id"],
+                    message: 'Expected number, received string'
+                  }
+            ]);
+        }
+        assert.isOk(didFail);
+    });
+
+    it('should fail to parse report with zero-length reportName', function() {
+        let didFail = false;
+        try {
+            const badprog
+                = reportSchema.parse(data.BADreports[1]);
+        } catch (err) {
+            didFail = true;
+            // console.log(err);
+            assert.deepEqual(err.issues, [
+                {
+                    code: 'too_small',
+                    minimum: 1,
+                    type: 'string',
+                    inclusive: true,
+                    exact: false,
+                    message: 'String must contain at least 1 character(s)',
+                    path: ["clientName"]
+                  } 
+            ]);
+        }
+        assert.isOk(didFail);
+    });
+
+    it('should fail to parse report with extra-long reportName', function() {
+        let didFail = false;
+        try {
+            const badprog
+                = reportSchema.parse(data.BADreports[2]);
+        } catch (err) {
+            didFail = true;
+            // console.log(err);
+            assert.deepEqual(err.issues, [
+                {
+                    code: "too_big",
+                    maximum: 128,
+                    type: 'string',
+                    inclusive: true,
+                    exact: false,
+                    message: 'String must contain at most 128 character(s)',
+                    path: ["clientName"]
+                  } 
+            ]);
+        }
+        assert.isOk(didFail);
+    });
+});

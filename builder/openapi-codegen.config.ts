3cc3f50422e3960a7286e7efcaf17d6484eab78d
David Herron <david@davidherron.com>
2023-12-14 16:45:50 +0200

    Initial revision

---
+++ builder/openapi-codegen.config.ts
@@ -0,0 +1,25 @@
+import {
+  generateSchemaTypes,
+  generateFetchers
+} from "@openapi-codegen/typescript";
+import {
+  defineConfig
+} from "@openapi-codegen/cli";
+export default defineConfig({
+  openADR: {
+    from: {
+      relativePath: "../oadr3.0.1.yaml",
+      source: "file",
+    },
+    outputDir: "./foo",
+    to: async (context) => {
+      const { schemasFiles } = await generateSchemaTypes(context, {
+        filenamePrefix: "openADR",
+      });
+      await generateFetchers(context, {
+        /* config */
+        schemasFiles,
+      });
+    },
+  },
+});

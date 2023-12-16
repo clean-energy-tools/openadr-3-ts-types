fdd37ffc66b9b9b5a7439f8121e53b03aec32aa2
David Herron <david@davidherron.com>
2023-12-16 22:05:46 +0200

    Start testing Joi code

--- test/lib/dateTime.ts
+++ test/lib/dateTime.ts
@@ -9,9 +9,11 @@ export const __dirname = path.dirname(__filename);
 
 import { promises as fsp } from 'node:fs';
 import {
-    DateTime, parseDateTime, Duration, parseDuration
+    DateTime, parseDateTime, Duration, parseDuration,
+    joiDateTime, joiDuration
 } from '../../package/dist/index.js';
 // from 'openadr-3-ts-types';
+import Joi from 'joi';
 import YAML from 'js-yaml';
 
 describe('DATE TIME DURATION', function() {
@@ -25,47 +27,85 @@ describe('DATE TIME DURATION', function() {
         data = doc;
     });
 
-    it('should parse dateTime', function() {
-        // console.log(data.dateTimes[0]);
-        const dateTime: DateTime = parseDateTime.parse(data.dateTimes[0]) as DateTime;
-        // console.log(dateTime);
-        assert.deepEqual(dateTime, "2023-02-20T00:00:00Z" as any);
+    describe('ZOD', function() {
+
+        it('should parse dateTime', function() {
+            // console.log(data.dateTimes[0]);
+            const dateTime: DateTime = parseDateTime.parse(data.dateTimes[0]) as DateTime;
+            // console.log(dateTime);
+            assert.deepEqual(dateTime, "2023-02-20T00:00:00Z" as any);
+        });
+
+        // it('should parse dateTime', function() {
+        //     // console.log(data.dateTimes[0]);
+        //     const dateTime: DateTime = dateTimeSchema.parse(data.dateTimes[1]) as DateTime;
+        //     // console.log(dateTime);
+        //     assert.deepEqual(dateTime, '1994-11-05T08:15:30-05:00' as any);
+        // });
+
+        it('should parse duration 0', function() {
+            // console.log(data.dateTimes[0]);
+            const dateTime: Duration = parseDuration.parse(data.durations[0]) as Duration;
+            // console.log(dateTime);
+            assert.deepEqual(dateTime, "P1Y2M3DT1H20M10S" as any);
+        });
+
+        it('should parse duration 1', function() {
+            // console.log(data.dateTimes[0]);
+            const dateTime: Duration = parseDuration.parse(data.durations[1]) as Duration;
+            // console.log(dateTime);
+            assert.deepEqual(dateTime, "P2M3DT1H20M10S" as any);
+        });
+
+        it('should parse duration 2', function() {
+            // console.log(data.dateTimes[0]);
+            const dateTime: Duration = parseDuration.parse(data.durations[2]) as Duration;
+            // console.log(dateTime);
+            assert.deepEqual(dateTime, "-P1Y2M3DT1H20M10S" as any);
+        });
+
+        it('should parse duration 3', function() {
+            // console.log(data.dateTimes[0]);
+            const dateTime: Duration = parseDuration.parse(data.durations[3]) as Duration;
+            // console.log(dateTime);
+            assert.deepEqual(dateTime, "-P2M3DT1H20M10S" as any);
+        });
     });
 
-    // it('should parse dateTime', function() {
-    //     // console.log(data.dateTimes[0]);
-    //     const dateTime: DateTime = dateTimeSchema.parse(data.dateTimes[1]) as DateTime;
-    //     // console.log(dateTime);
-    //     assert.deepEqual(dateTime, '1994-11-05T08:15:30-05:00' as any);
-    // });
+    describe('JOI', function() {
 
-    it('should parse duration 0', function() {
-        // console.log(data.dateTimes[0]);
-        const dateTime: Duration = parseDuration.parse(data.durations[0]) as Duration;
-        // console.log(dateTime);
-        assert.deepEqual(dateTime, "P1Y2M3DT1H20M10S" as any);
-    });
+        it('should parse dateTime', function() {
+            // console.log(data.dateTimes[0]);
+            Joi.assert(data.dateTimes[0], joiDateTime);
+        });
 
-    it('should parse duration 1', function() {
-        // console.log(data.dateTimes[0]);
-        const dateTime: Duration = parseDuration.parse(data.durations[1]) as Duration;
-        // console.log(dateTime);
-        assert.deepEqual(dateTime, "P2M3DT1H20M10S" as any);
-    });
+        // it('should parse dateTime', function() {
+        //     // console.log(data.dateTimes[0]);
+        //     const dateTime: DateTime = dateTimeSchema.parse(data.dateTimes[1]) as DateTime;
+        //     // console.log(dateTime);
+        //     assert.deepEqual(dateTime, '1994-11-05T08:15:30-05:00' as any);
+        // });
 
-    it('should parse duration 2', function() {
-        // console.log(data.dateTimes[0]);
-        const dateTime: Duration = parseDuration.parse(data.durations[2]) as Duration;
-        // console.log(dateTime);
-        assert.deepEqual(dateTime, "-P1Y2M3DT1H20M10S" as any);
-    });
+        it('should parse duration 0', function() {
+            // console.log(data.dateTimes[0]);
+            Joi.assert(data.durations[0], joiDuration);
+        });
 
-    it('should parse duration 3', function() {
-        // console.log(data.dateTimes[0]);
-        const dateTime: Duration = parseDuration.parse(data.durations[3]) as Duration;
-        // console.log(dateTime);
-        assert.deepEqual(dateTime, "-P2M3DT1H20M10S" as any);
-    });
+        it('should parse duration 1', function() {
+            // console.log(data.dateTimes[0]);
+            Joi.assert(data.durations[1], joiDuration);
+        });
+
+        it('should parse duration 2', function() {
+            // console.log(data.dateTimes[0]);
+            Joi.assert(data.durations[2], joiDuration);
+        });
+
+        it('should parse duration 3', function() {
+            // console.log(data.dateTimes[0]);
+            Joi.assert(data.durations[3], joiDuration);
+        });
+    })
 
 });
 
--- test/lib/event.ts
+++ test/lib/event.ts
@@ -9,9 +9,10 @@ export const __dirname = path.dirname(__filename);
 
 import { promises as fsp } from 'node:fs';
 import {
-    Event, parseEvent
+    Event, parseEvent, joiEvent
 } from '../../package/dist/index.js';
 // from 'openadr-3-ts-types';
+import Joi from 'joi';
 import YAML from 'js-yaml';
 
 
@@ -26,206 +27,322 @@ describe('EVENT', function() {
         data = doc;
     });
 
-    it('should parse event', function() {
-        // console.log(`to parse`, data.events[0]);
-        const event: Event = parseEvent.parse(data.events[0]) as Event;
-        // console.log(event);
-        assert.deepEqual(event, {
-            programID: '0',
-            eventName: 'First event',
-            priority: 99,
-            targets: [ { 
-                type: 'GROUP_NAME', values: ["VEN99"]
-            } ],
-            reportDescriptors: [
-              {
-                payloadType: 'USAGE',
-                readingType: 'DIRECT_READ',
-                units: null,
-                targets: [{
-                    type: "GROUP_NAME",
-                    values: [ "VEN99" ]
-                }],
-                aggregate: false,
-                startInterval: -1,
-                numIntervals: -1,
-                historical: true,
-                frequency: -1,
-                repeat: 1
-              }
-            ],
-            payloadDescriptors: null,
-            intervalPeriod: {
-                start: '2023-02-20T00:00:00Z',
-                duration: 'P3M',
-                randomizeStart: "PT0S"
-            },
-            intervals: [ {
-                id: 0,
-                payloads: [ {
-                    type: "EMPTY",
-                    values: []
-                }]
-            } ]
-          });
-    });
+    describe('ZOD', function() {
 
-    it('should parse event with extra fields and passthrough', function() {
-        const event: Event = parseEvent.passthrough().parse(data.events[0]) as Event;
-        // console.log(event);
-        assert.deepEqual(event, {
-            programID: '0',
-            eventName: 'First event',
-            extraField: 'With unknown value',
-            priority: 99,
-            targets: [ { 
-                type: 'GROUP_NAME', values: ["VEN99"]
-            } ],
-            reportDescriptors: [
-              {
-                payloadType: 'USAGE',
-                readingType: 'DIRECT_READ',
-                units: null,
-                targets: [{
-                    type: "GROUP_NAME",
-                    values: [ "VEN99" ]
-                }],
-                aggregate: false,
-                startInterval: -1,
-                numIntervals: -1,
-                historical: true,
-                frequency: -1,
-                repeat: 1
-              }
-            ],
-            payloadDescriptors: null,
-            intervalPeriod: {
-                start: '2023-02-20T00:00:00Z',
-                duration: 'P3M',
-                randomizeStart: "PT0S"
-            },
-            intervals: [ {
-                id: 0,
-                payloads: [ {
-                    type: "EMPTY",
-                    values: []
-                }]
-            } ]
-          } as any);
-    });
-
-    it('should fail to parse event with extra fields and strict', function() {
-        let didFail = false;
-        try {
-            const event: Event = parseEvent.strict().parse(data.events[0]) as Event;
-        } catch (err: any) {
-            didFail = true;
-            // console.log(err);
-            assert.deepEqual(err.issues, [
+        it('should parse event', function() {
+            // console.log(`to parse`, data.events[0]);
+            const event: Event = parseEvent.parse(data.events[0]) as Event;
+            // console.log(event);
+            assert.deepEqual(event, {
+                programID: '0',
+                eventName: 'First event',
+                priority: 99,
+                targets: [ { 
+                    type: 'GROUP_NAME', values: ["VEN99"]
+                } ],
+                reportDescriptors: [
                 {
-                  code: 'unrecognized_keys',
-                  keys: [
-                    "extraField"
-                  ],
-                  path: [],
-                  message: "Unrecognized key(s) in object: 'extraField'"
+                    payloadType: 'USAGE',
+                    readingType: 'DIRECT_READ',
+                    units: null,
+                    targets: [{
+                        type: "GROUP_NAME",
+                        values: [ "VEN99" ]
+                    }],
+                    aggregate: false,
+                    startInterval: -1,
+                    numIntervals: -1,
+                    historical: true,
+                    frequency: -1,
+                    repeat: 1
                 }
-              ])
-        }
-        assert.isOk(didFail);
-    });
+                ],
+                payloadDescriptors: null,
+                intervalPeriod: {
+                    start: '2023-02-20T00:00:00Z',
+                    duration: 'P3M',
+                    randomizeStart: "PT0S"
+                },
+                intervals: [ {
+                    id: 0,
+                    payloads: [ {
+                        type: "EMPTY",
+                        values: []
+                    }]
+                } ]
+            });
+        });
 
-    it('should parse event with default values', function() {
-        const event: Event = parseEvent.parse(data.eventsDefaults[0]) as Event;
-        // console.log(event);
-        assert.deepEqual(event, {
-            programID: '0',
-            eventName: 'Event with defaults',
-            priority: null,
-            targets: null,
-            reportDescriptors: null,
-            payloadDescriptors: null,
-            intervalPeriod: {
-                start: '2023-02-20T00:00:00Z',
-                duration: 'P3M',
-                randomizeStart: "PT0S"
-            },
-            intervals: [ {
-                id: 0,
-                payloads: [ {
-                    type: "EMPTY",
-                    values: []
-                }]
-            } ]
-          } as any);
-    });
-
-    it('should fail to parse event wtih bad values', function() {
-        let didFail = false;
-        try {
-            const event: Event = parseEvent.parse(data.eventsBadValues[0]) as Event;
-        } catch (err: any) {
-            didFail = true;
-            // console.log(err);
-            assert.deepEqual(err.issues, [
+        it('should parse event with extra fields and passthrough', function() {
+            const event: Event = parseEvent.passthrough().parse(data.events[0]) as Event;
+            // console.log(event);
+            assert.deepEqual(event, {
+                programID: '0',
+                eventName: 'First event',
+                extraField: 'With unknown value',
+                priority: 99,
+                targets: [ { 
+                    type: 'GROUP_NAME', values: ["VEN99"]
+                } ],
+                reportDescriptors: [
                 {
-                    "code": "invalid_type",
-                    "expected": "string",
-                    "received": "number",
-                    "path": [
-                      "programID"
-                    ],
-                    "message": "Expected string, received number"
-                },
-                {
-                    "code": "too_small",
-                    "minimum": 0,
-                    "type": "number",
-                    "inclusive": true,
-                    "exact": false,
-                    "message": "Number must be greater than or equal to 0",
-                    "path": [
-                      "priority"
-                    ]
-                },
-                {
-                    "code": "invalid_type",
-                    "expected": "array",
-                    "received": "string",
-                    "path": [
-                      "targets"
-                    ],
-                    "message": "Expected array, received string"
-                },
-                {
-                    "code": "invalid_type",
-                    "expected": "array",
-                    "received": "string",
-                    "path": [
-                      "reportDescriptors"
-                    ],
-                    "message": "Expected array, received string"
-                },
-                {
-                    "code": "invalid_type",
-                    "expected": "array",
-                    "received": "string",
-                    "path": [
-                      "payloadDescriptors"
-                    ],
-                    "message": "Expected array, received string"
-                },
-                {
-                    "code": "invalid_type",
-                    "expected": "array",
-                    "received": "string",
-                    "path": [
-                      "intervals"
-                    ],
-                    "message": "Expected array, received string"
+                    payloadType: 'USAGE',
+                    readingType: 'DIRECT_READ',
+                    units: null,
+                    targets: [{
+                        type: "GROUP_NAME",
+                        values: [ "VEN99" ]
+                    }],
+                    aggregate: false,
+                    startInterval: -1,
+                    numIntervals: -1,
+                    historical: true,
+                    frequency: -1,
+                    repeat: 1
                 }
-            ]);
-        }
-        assert.isOk(didFail);
+                ],
+                payloadDescriptors: null,
+                intervalPeriod: {
+                    start: '2023-02-20T00:00:00Z',
+                    duration: 'P3M',
+                    randomizeStart: "PT0S"
+                },
+                intervals: [ {
+                    id: 0,
+                    payloads: [ {
+                        type: "EMPTY",
+                        values: []
+                    }]
+                } ]
+            } as any);
+        });
+
+        it('should fail to parse event with extra fields and strict', function() {
+            let didFail = false;
+            try {
+                const event: Event = parseEvent.strict().parse(data.events[0]) as Event;
+            } catch (err: any) {
+                didFail = true;
+                // console.log(err);
+                assert.deepEqual(err.issues, [
+                    {
+                    code: 'unrecognized_keys',
+                    keys: [
+                        "extraField"
+                    ],
+                    path: [],
+                    message: "Unrecognized key(s) in object: 'extraField'"
+                    }
+                ])
+            }
+            assert.isOk(didFail);
+        });
+
+        it('should parse event with default values', function() {
+            const event: Event = parseEvent.parse(data.eventsDefaults[0]) as Event;
+            // console.log(event);
+            assert.deepEqual(event, {
+                programID: '0',
+                eventName: 'Event with defaults',
+                priority: null,
+                targets: null,
+                reportDescriptors: null,
+                payloadDescriptors: null,
+                intervalPeriod: {
+                    start: '2023-02-20T00:00:00Z',
+                    duration: 'P3M',
+                    randomizeStart: "PT0S"
+                },
+                intervals: [ {
+                    id: 0,
+                    payloads: [ {
+                        type: "EMPTY",
+                        values: []
+                    }]
+                } ]
+            } as any);
+        });
+
+        it('should fail to parse event wtih bad values', function() {
+            let didFail = false;
+            try {
+                const event: Event = parseEvent.parse(data.eventsBadValues[0]) as Event;
+            } catch (err: any) {
+                didFail = true;
+                // console.log(err);
+                assert.deepEqual(err.issues, [
+                    {
+                        "code": "invalid_type",
+                        "expected": "string",
+                        "received": "number",
+                        "path": [
+                        "programID"
+                        ],
+                        "message": "Expected string, received number"
+                    },
+                    {
+                        "code": "too_small",
+                        "minimum": 0,
+                        "type": "number",
+                        "inclusive": true,
+                        "exact": false,
+                        "message": "Number must be greater than or equal to 0",
+                        "path": [
+                        "priority"
+                        ]
+                    },
+                    {
+                        "code": "invalid_type",
+                        "expected": "array",
+                        "received": "string",
+                        "path": [
+                        "targets"
+                        ],
+                        "message": "Expected array, received string"
+                    },
+                    {
+                        "code": "invalid_type",
+                        "expected": "array",
+                        "received": "string",
+                        "path": [
+                        "reportDescriptors"
+                        ],
+                        "message": "Expected array, received string"
+                    },
+                    {
+                        "code": "invalid_type",
+                        "expected": "array",
+                        "received": "string",
+                        "path": [
+                        "payloadDescriptors"
+                        ],
+                        "message": "Expected array, received string"
+                    },
+                    {
+                        "code": "invalid_type",
+                        "expected": "array",
+                        "received": "string",
+                        "path": [
+                        "intervals"
+                        ],
+                        "message": "Expected array, received string"
+                    }
+                ]);
+            }
+            assert.isOk(didFail);
+        });
+    });
+
+    describe('JOI', function() {
+
+        it('should parse event', function() {
+            // console.log(`to parse`, data.events[0]);
+            Joi.assert(data.events[0], joiEvent);
+        });
+
+        it('should parse event with extra fields and passthrough', function() {
+            Joi.assert(data.events[0], joiEvent, { allowUnknown: true });
+        });
+
+        it('should fail to parse event with extra fields and strict', function() {
+            Joi.assert(data.events[0], joiEvent, { stripUnknown: true });
+        });
+
+        it('should parse event with default values', function() {
+            Joi.assert(data.eventsDefaults[0], joiEvent);
+            // TODO ensure the defaults are correct
+
+            // const event: Event = parseEvent.parse(data.eventsDefaults[0]) as Event;
+            // // console.log(event);
+            // assert.deepEqual(event, {
+            //     programID: '0',
+            //     eventName: 'Event with defaults',
+            //     priority: null,
+            //     targets: null,
+            //     reportDescriptors: null,
+            //     payloadDescriptors: null,
+            //     intervalPeriod: {
+            //         start: '2023-02-20T00:00:00Z',
+            //         duration: 'P3M',
+            //         randomizeStart: "PT0S"
+            //     },
+            //     intervals: [ {
+            //         id: 0,
+            //         payloads: [ {
+            //             type: "EMPTY",
+            //             values: []
+            //         }]
+            //     } ]
+            // } as any);
+        });
+
+        // it('should fail to parse event wtih bad values', function() {
+        //     let didFail = false;
+        //     try {
+        //         const event: Event = parseEvent.parse(data.eventsBadValues[0]) as Event;
+        //     } catch (err: any) {
+        //         didFail = true;
+        //         // console.log(err);
+        //         assert.deepEqual(err.issues, [
+        //             {
+        //                 "code": "invalid_type",
+        //                 "expected": "string",
+        //                 "received": "number",
+        //                 "path": [
+        //                 "programID"
+        //                 ],
+        //                 "message": "Expected string, received number"
+        //             },
+        //             {
+        //                 "code": "too_small",
+        //                 "minimum": 0,
+        //                 "type": "number",
+        //                 "inclusive": true,
+        //                 "exact": false,
+        //                 "message": "Number must be greater than or equal to 0",
+        //                 "path": [
+        //                 "priority"
+        //                 ]
+        //             },
+        //             {
+        //                 "code": "invalid_type",
+        //                 "expected": "array",
+        //                 "received": "string",
+        //                 "path": [
+        //                 "targets"
+        //                 ],
+        //                 "message": "Expected array, received string"
+        //             },
+        //             {
+        //                 "code": "invalid_type",
+        //                 "expected": "array",
+        //                 "received": "string",
+        //                 "path": [
+        //                 "reportDescriptors"
+        //                 ],
+        //                 "message": "Expected array, received string"
+        //             },
+        //             {
+        //                 "code": "invalid_type",
+        //                 "expected": "array",
+        //                 "received": "string",
+        //                 "path": [
+        //                 "payloadDescriptors"
+        //                 ],
+        //                 "message": "Expected array, received string"
+        //             },
+        //             {
+        //                 "code": "invalid_type",
+        //                 "expected": "array",
+        //                 "received": "string",
+        //                 "path": [
+        //                 "intervals"
+        //                 ],
+        //                 "message": "Expected array, received string"
+        //             }
+        //         ]);
+        //     }
+        //     assert.isOk(didFail);
+        // });
     });
 });

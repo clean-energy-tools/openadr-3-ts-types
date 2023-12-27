4cf08fefd6c28cbe961fcfe5d700bb2ecd571233
David Herron <david@davidherron.com>
2023-12-27 17:25:51 +0200

    JOI tests for Points

--- test/lib/point.ts
+++ test/lib/point.ts
@@ -9,7 +9,7 @@ export const __dirname = path.dirname(__filename);
 
 import { promises as fsp } from 'node:fs';
 import {
-    Point, parsePoint
+    Point, parsePoint, joiPoint
 } from '../../package/dist/index.js';
 // from 'openadr-3-ts-types';
 import YAML from 'js-yaml';
@@ -26,109 +26,265 @@ describe('POINT', function() {
         data = doc;
     });
 
-    it('should parse point', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.parse(data.points[0]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: 1, y: 1 });
+    describe('ZOD', function() {
+
+        it('should parse point', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.parse(data.points[0]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: 1, y: 1 });
+        });
+
+        it('should parse point w/ negative numbers', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.parse(data.points[1]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: -1, y: -1 });
+        });
+
+        it('should parse point w/ floating numbers', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.parse(data.points[2]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: 1.1, y: 2.2 });
+        });
+
+        it('should parse point w/ missing x', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.parse(data.points[3]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: null, y: 3 });
+        });
+
+        it('should parse point w/ missing y', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.parse(data.points[4]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: 4, y: null });
+        });
+
+        it('should parse point w/ both missing', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.parse(data.points[5]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: null, y: null });
+        });
+
+        it('should parse point w/ extra data not seen', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.parse(data.points[6]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: 1, y: 2 });
+        });
+
+        it('should parse point w/ extra data seen w/ passthrough', function() {
+            // console.log(data.intervals[0]);
+            const point: Point = parsePoint.passthrough().parse(data.points[6]) as Point;
+            // console.log(point);
+            assert.deepEqual(point, { x: 1, y: 2, extraData: 'one' } as any);
+        });
+
+        it('should fail to parse point w/ extra data w/ strict', function() {
+            // console.log(data.intervals[0]);
+            let didFail = false;
+            try {
+                const point: Point = parsePoint.strict().parse(data.points[6]) as Point;
+            } catch (err: any) {
+                didFail = true;
+                // console.log(err.issues);
+                assert.deepEqual(err.issues, [
+                    {
+                    code: 'unrecognized_keys',
+                    keys: [ 'extraData' ],
+                    path: [],
+                    message: "Unrecognized key(s) in object: 'extraData'"
+                    }
+                ]);
+            }
+            assert.isOk(didFail);
+        });
+
+        it('should fail to parse point w/ bad data', function() {
+            // console.log(data.intervals[0]);
+            let didFail = false;
+            try {
+                const point: Point = parsePoint.strict().parse(data.points[7]) as Point;
+            } catch (err: any) {
+                didFail = true;
+                // console.log(err.issues);
+                assert.deepEqual(err.issues, [
+                    {
+                    code: 'invalid_type',
+                    expected: 'number',
+                    received: 'string',
+                    path: [ 'x' ],
+                    message: 'Expected number, received string'
+                    },
+                    {
+                    code: 'invalid_type',
+                    expected: 'number',
+                    received: 'string',
+                    path: [ 'y' ],
+                    message: 'Expected number, received string'
+                    }
+                ]);
+            }
+            assert.isOk(didFail);
+        });
+
     });
 
-    it('should parse point w/ negative numbers', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.parse(data.points[1]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: -1, y: -1 });
-    });
+    describe('JOI', function() {
 
-    it('should parse point w/ floating numbers', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.parse(data.points[2]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: 1.1, y: 2.2 });
-    });
+        it('should parse point', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[0]);
 
-    it('should parse point w/ missing x', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.parse(data.points[3]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: null, y: 3 });
-    });
+            // console.log(result.value);
 
-    it('should parse point w/ missing y', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.parse(data.points[4]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: 4, y: null });
-    });
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error === 'undefined');
+            assert.isOk(typeof result.value === 'object');
 
-    it('should parse point w/ both missing', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.parse(data.points[5]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: null, y: null });
-    });
+            // console.log(result.value);
 
-    it('should parse point w/ extra data not seen', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.parse(data.points[6]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: 1, y: 2 });
-    });
+            assert.deepEqual(result.value, { x: 1, y: 1 });
+        });
 
-    it('should parse point w/ extra data seen w/ passthrough', function() {
-        // console.log(data.intervals[0]);
-        const point: Point = parsePoint.passthrough().parse(data.points[6]) as Point;
-        // console.log(point);
-        assert.deepEqual(point, { x: 1, y: 2, extraData: 'one' } as any);
-    });
+        it('should parse point w/ negative numbers', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[1]);
 
-    it('should fail to parse point w/ extra data w/ strict', function() {
-        // console.log(data.intervals[0]);
-        let didFail = false;
-        try {
-            const point: Point = parsePoint.strict().parse(data.points[6]) as Point;
-        } catch (err: any) {
-            didFail = true;
-            // console.log(err.issues);
-            assert.deepEqual(err.issues, [
-                {
-                  code: 'unrecognized_keys',
-                  keys: [ 'extraData' ],
-                  path: [],
-                  message: "Unrecognized key(s) in object: 'extraData'"
-                }
-            ]);
-        }
-        assert.isOk(didFail);
-    });
+            // console.log(result.value);
 
-    it('should fail to parse point w/ bad data', function() {
-        // console.log(data.intervals[0]);
-        let didFail = false;
-        try {
-            const point: Point = parsePoint.strict().parse(data.points[7]) as Point;
-        } catch (err: any) {
-            didFail = true;
-            // console.log(err.issues);
-            assert.deepEqual(err.issues, [
-                {
-                  code: 'invalid_type',
-                  expected: 'number',
-                  received: 'string',
-                  path: [ 'x' ],
-                  message: 'Expected number, received string'
-                },
-                {
-                  code: 'invalid_type',
-                  expected: 'number',
-                  received: 'string',
-                  path: [ 'y' ],
-                  message: 'Expected number, received string'
-                }
-            ]);
-        }
-        assert.isOk(didFail);
-    });
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error === 'undefined');
+            assert.isOk(typeof result.value === 'object');
 
+            // console.log(result.value);
+
+            assert.deepEqual(result.value, { x: -1, y: -1 });
+        });
+
+        it('should parse point w/ floating numbers', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[2]);
+
+            // console.log(result.value);
+
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error === 'undefined');
+            assert.isOk(typeof result.value === 'object');
+
+            // console.log(result.value);
+
+            assert.deepEqual(result.value, { x: 1.1, y: 2.2 });
+        });
+
+        it('should parse point w/ missing x', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[3]);
+
+            // console.log(result.value);
+
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error !== 'undefined');
+            assert.isOk(typeof result.value === 'object');
+
+            // console.log(result.value);
+
+            assert.deepEqual(result.value, { y: 3 });
+            assert.equal(result.error.message,
+                '"x" is required');
+        });
+
+        it('should parse point w/ missing y', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[4]);
+
+            // console.log(result.value);
+
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error !== 'undefined');
+            assert.isOk(typeof result.value === 'object');
+
+            // console.log(result.value);
+
+            assert.deepEqual(result.value, { x: 4 });
+            assert.equal(result.error.message,
+                '"y" is required');
+        });
+
+        it('should parse point w/ both missing', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[5]);
+
+            // console.log(result.value);
+
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error !== 'undefined');
+            assert.isOk(typeof result.value === 'object');
+
+            // console.log(result.value);
+
+            assert.deepEqual(result.value, { });
+            assert.equal(result.error.message,
+                '"x" is required');
+        });
+
+        it('should parse point w/ extra data seen w/ passthrough', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[6]);
+
+            // console.log(result.value);
+
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error === 'undefined');
+            assert.isOk(typeof result.value === 'object');
+
+            // console.log(result.value);
+
+            assert.deepEqual(result.value, {
+                x: 1,
+                y: 2,
+                extraData: 'one'
+            });
+        });
+
+        it('should fail to parse point w/ bad data', function() {
+            // console.log(data.intervals[0]);
+            const result = joiPoint.validate(data.points[7]);
+
+            // console.log(result.value);
+
+            if (result.error) {
+                // console.log(result.error);
+            }
+            assert.isOk(typeof result.error !== 'undefined');
+            assert.isOk(typeof result.value === 'object');
+
+            // console.log(result.value);
+
+            assert.deepEqual(result.value, {
+                x: 'one', y: 'two'
+            });
+            assert.equal(result.error.message,
+                '"x" must be a number');
+        });
+    });
 
 });
